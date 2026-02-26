e0395b494b7fc65134ce508f2fc3b4cdc240d4db
shaurya-s001 <shaurya-s001@webreinvent.com>
2026-02-26 11:31:53 +0530

    Nuxt.js life cycle hooks revised and implemented

---
+++ app/pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+Hello - Hi - Shaurya
+
+    </template>
---
+++ app/pages/life-cycle.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, onUnmounted,onMounted,onUpdated } from 'vue';
+const count = ref(0);
+
+onMounted(()=>{
+    console.log('component mounted')
+})
+onUpdated(()=>{
+    console.log('updated the code')
+})
+
+onUnmounted(()=>{
+    console.log('onUnmounted code')
+})
+
+const increaseCount = () => {
+count.value++;
+}
+
+</script>
+
+<template>
+<div >
+
+<button @click="increaseCount">{{count}}</button>
+</div>
+
+</template>
