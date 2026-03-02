be2e0ed3be67d3e7d5669561f27ada4e2810fd38
shaurya-s001 <shaurya-s001@webreinvent.com>
2026-03-02 12:21:02 +0530

    directives added

---
+++ Vue-Practice/rev/src/components/EventHandling.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+       
+        <button  v-once v-on:click="increasecounter">            {{ count  }}
+</button>
+       
+<h1 >
+    Event Handling
+</h1>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const count = ref(0);
+const htmlleramera = '<h1>THis is my html head</h1>'
+
+const increasecounter = () =>{
+   count.value++
+}
+
+</script>
