d4d3d1baca9c56e5406c1cbdf36f1471ccb885f7
shaurya-s001 <shaurya-s001@webreinvent.com>
2026-03-03 11:55:47 +0530

    todo code added

--- Vue-Practice/rev/src/App.vue
+++ Vue-Practice/rev/src/App.vue
@@ -1,13 +1,47 @@
-<script setup lang="ts">
-
-
-
-</script>
-
 <template>
-<EventHandling/>
-<TemPlateref/>
-<Parent/>
-<child/>
+  <div class="container">
+    <h1>Vue 3 To-Do List</h1>
+
+    <input
+      v-model="newTodo"
+      @keyup.enter="addTodo"
+      type="text"
+      placeholder="Add a new todo"
+    />
+
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        <input type="checkbox" v-model="todo.completed" />
+
+        <span :class="{ completed: todo.completed }">
+          {{ todo.text }}
+        </span>
+
+        <button @click="removeTodo(todo.id)">Delete</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
+<script setup>
+import { ref } from 'vue'
+
+const newTodo = ref('')
+const todos = ref([])
+
+function addTodo() {
+  if (newTodo.value.trim() === '') return
+
+  todos.value.push({
+    id: Date.now(),
+    text: newTodo.value.trim(),
+    completed: false
+  })
+
+  newTodo.value = ''
+}
+
+function removeTodo(id) {
+  todos.value = todos.value.filter(todo => todo.id !== id)
+}
+</script>
---
+++ Vue-Practice/rev/src/components/Addtodo.vue
@@ -0,0 +1,22 @@
+<template>
+<div><input 
+      v-model="newTodo" 
+      @keyup.enter="addTodo" 
+      type="text" 
+      placeholder="Add a new todo" 
+    />hi</div>
+      
+
+
+
+    </template>
+
+
+<script setup>
+import { onDeactivated } from 'vue';
+
+const addreference = ref([]);
+
+
+
+</script>
